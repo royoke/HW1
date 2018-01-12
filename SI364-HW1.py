77a4900ff7af7e08bf06ca510e68547778e229b3
Teddy Okerstrom <royoke@umich.edu>
2018-01-12 15:07:11 -0500

    adding problems 3 and 4

--- SI364-HW1.py
+++ SI364-HW1.py
@@ -45,3 +45,32 @@ if __name__ == '__main__':
 ## Of course, you'll also need the requests library and knowledge of how to make a request to a REST API for data.
 
 ## Run the app locally (repeatedly) and try these URLs out!
+
+## [PROBLEM 3] - 250 points
+
+## Edit the above Flask application code so that if you run the application locally and got to the URL http://localhost:5000/question, you see a form that asks you to enter your favorite number.
+## Once you enter a number and submit it to the form, you should then see a web page that says "Double your favorite number is <number>". For example, if you enter 2 into the form, you should then see a page that says "Double your favorite number is 4". Careful about types in your Python code!
+## You can assume a user will always enter a number only.
+
+
+## [PROBLEM 4] - 350 points
+
+## Come up with your own interactive data exchange that you want to see happen dynamically in the Flask application, and build it into the above code for a Flask application, following a few requirements.
+
+## You should create a form that appears at the route: http://localhost:5000/problem4form
+
+## Submitting the form should result in your seeing the results of the form on the same page.
+
+## What you do for this problem should:
+# - not be an exact repeat of something you did in class
+# - must include an HTML form with checkboxes and text entry
+# - should, on submission of data to the HTML form, show new data that depends upon the data entered into the submission form and is readable by humans (more readable than e.g. the data you got in Problem 2 of this HW). The new data should be gathered via API request or BeautifulSoup.
+
+# You should feel free to be creative and do something fun for you --
+# And use this opportunity to make sure you understand these steps: if you think going slowly and carefully writing out steps for a simpler data transaction, like Problem 1, will help build your understanding, you should definitely try that!
+
+# You can assume that a user will give you the type of input/response you expect in your form; you do not need to handle errors or user confusion. (e.g. if your form asks for a name, you can assume a user will type a reasonable name; if your form asks for a number, you can assume a user will type a reasonable number; if your form asks the user to select a checkbox, you can assume they will do that.)
+
+# Points will be assigned for each specification in the problem.
+
+
